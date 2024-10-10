f43f226586f3d7bb585fb728eefdd56a51fbd218
51750 <4531076+51750@users.noreply.github.com>
2024-10-10 22:04:15 +0300

    Update README.md with project description and TODO list

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # My Spending
+A web application designed to track supermarket expenses. It automatically captures product information by scanning supermarket receipts, helping you with expense tracking, analyzing your spending patterns, and offering additional features to manage your finances efficiently.
+
+
+## TODO
+Here are the tasks we need to do.
+
+### Models
+- [ ] Receipt: `storeName`, `storeAddress`, `totalPrice`, `date`
+- [ ] ReceiptProduct: `receipt`, `name`, `nameEnglish`, `nameChinese`, `price`, `discount`
+
+### views
+- [ ] Receipt List View: Display a list of all receipts with basic details like store name, date, and total price.
+- [ ] Receipt Detail View: Display detailed information of a selected receipt, including all products, their prices, and any discounts applied.
+- [ ] Add Receipt View: A form to add a new receipt manually, including fields for store name, store address, total price, and date.
+- [ ] Edit Receipt View: A form to edit an existing receipt's details.
+
+### URLs
+- [ ] `/receipts/`: URL to view the list of receipts.
+- [ ] `/receipts/<id>/`: URL to view the details of a specific receipt.
+- [ ] `/receipts/add/`: URL to add a new receipt.
+- [ ] `/receipts/<id>/edit/`: URL to edit an existing receipt.
--- lib/gemini.py
+++ lib/gemini.py
@@ -46,20 +46,22 @@ chat_session = model.start_chat(
         {
             "role": "user",
             "parts": [
-                "你是一个购物小票识别程序，我会上传照片，你识别后返回给我JSON，完需要如下信息：超市名称，超市地址，商品名称，商品翻译成英文的名称，商品翻译成中文的名称，商品价格，小票总价，时间日期",
+                "你是一个购物小票识别程序，我会上传照片，你识别后返回给我JSON，完需要如下信息：超市名称，超市地址，商品名称，商品翻译成英文的名称，商品翻译成中文的名称，商品价格，商品折扣，小票总价，时间日期",
             ],
         },
         {
             "role": "model",
             "parts": [
-                "```json\n{\n  \"storeName\": \"LIDL\",\n  \"storeAddress\": \"Lidl Suomi Ky Turku-Keskusta Eerikinkatu 4\",\n  \"items\": [\n    {\n      \"name\": \"Rostin Bao Bun höyry­sämpy­lä\",\n      \"nameEnglish\": \"Steamed Bao Bun\",\n      \"nameChinese\": \"蒸笼包\",\n      \"price\": 2.39\n    },\n    {\n      \"name\": \"Viini­ry­pä­le­tu­m­ma 500g\",\n      \"nameEnglish\": \"Dark Grapes 500g\",\n      \"nameChinese\": \"黑葡萄 500g\",\n      \"price\": 1.59\n    },\n    {\n      \"name\": \"Pohjolan Meijeri Täys­maito E\",\n      \"nameEnglish\": \"Full Milk E\",\n      \"nameChinese\": \"全脂牛奶\",\n      \"price\": 0.99\n    },\n    {\n      \"name\": \"Vit­a­sia­g­yo­za tai­ki­nan­yy­t. 1l\",\n      \"nameEnglish\": \"Gyoza Dough 1L\",\n      \"nameChinese\": \"饺子皮 1L\",\n      \"price\": 2.99\n    },\n    {\n      \"name\": \"Cosmo­po­li­tan salaatti 225g\",\n      \"nameEnglish\": \"Cosmopolitan Salad 225g\",\n      \"nameChinese\": \"大都会沙拉 225g\",\n      \"price\": 1.18\n    }\n  ],\n  \"totalPrice\": 8.32,\n  \"date\": \"2024-09-26T20:13:00Z\"\n}\n",
+                "```json\n{\n  \"storeName\": \"LIDL\",\n  \"storeAddress\": \"Lidl Suomi Ky Turku-Keskusta Eerikinkatu 4\",\n  \"items\": [\n    {\n      \"name\": \"Rostin Bao Bun höyry­sämpy­lä\",\n      \"nameEnglish\": \"Steamed Bao Bun\",\n      \"nameChinese\": \"蒸笼包\",\n      \"price\": 2.39,\n      \"discount\": 0.29,\n    },\n    {\n      \"name\": \"Viini­ry­pä­le­tu­m­ma 500g\",\n      \"nameEnglish\": \"Dark Grapes 500g\",\n      \"nameChinese\": \"黑葡萄 500g\",\n      \"price\": 1.59\n    },\n    {\n      \"name\": \"Pohjolan Meijeri Täys­maito E\",\n      \"nameEnglish\": \"Full Milk E\",\n      \"nameChinese\": \"全脂牛奶\",\n      \"price\": 0.99\n    },\n    {\n      \"name\": \"Vit­a­sia­g­yo­za tai­ki­nan­yy­t. 1l\",\n      \"nameEnglish\": \"Gyoza Dough 1L\",\n      \"nameChinese\": \"饺子皮 1L\",\n      \"price\": 2.99\n    },\n    {\n      \"name\": \"Cosmo­po­li­tan salaatti 225g\",\n      \"nameEnglish\": \"Cosmopolitan Salad 225g\",\n      \"nameChinese\": \"大都会沙拉 225g\",\n      \"price\": 1.18\n    }\n  ],\n  \"totalPrice\": 8.32,\n  \"date\": \"2024-09-26T20:13:00Z\"\n}\n",
             ],
         },
     ]
 )
 
 response = chat_session.send_message(upload_to_gemini(os.path.join(current_directory,
-                                                                   "IMG_4111.jpeg"), mime_type="image/jpeg"),)
+                                                                   "IMG_3851.jpeg"), mime_type="image/jpeg"),)
+
+print(response.text)
 
 python_dict = json.loads(response.text)
 
