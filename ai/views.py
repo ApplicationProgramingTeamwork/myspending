a36c4209ff7f4f78475ba6cfc4dea76a70107aeb
51750 <4531076+51750@users.noreply.github.com>
2024-10-11 22:26:22 +0300

    Adjust styles

--- ai/views.py
+++ ai/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.urls import reverse
 from ai.forms import PhotoUploadForm
 from ai.gemini import send_photo_to_gemini
+from dateutil import parser
+import pytz
 import json
 
 
@@ -19,6 +21,8 @@ def upload_photo(request):
     jsonString = request.GET.get('json', '')
     if jsonString:
         data = json.loads(jsonString)
-    else:
-        data = None
+        parsed_datetime = parser.isoparse(data['date'])
+        user_timezone = request.session.get('user_timezone', 'UTC')
+        local_tz = pytz.timezone(user_timezone)
+        data['date'] = parsed_datetime.astimezone(local_tz)
     return render(request, 'ai/index.html', {'data': data, 'language': languages[0]})
