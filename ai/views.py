e2671105923d60ce96dcbd2881f1b8a29b577392
51750 <4531076+51750@users.noreply.github.com>
2024-10-25 17:10:33 +0300

    Update spinner position in upload_photo.html template

--- ai/views.py
+++ ai/views.py
@@ -4,7 +4,7 @@ from ai.forms import PhotoUploadForm
 from ai.gemini import send_photo_to_gemini
 from django.contrib.auth.decorators import login_required
 from render.forms import ProductForm, ReceiptForm
-from django.utils import timezone
+from dateutil import parser
 import json
 
 
@@ -17,10 +17,10 @@ def upload_photo(request):
             json_string = send_photo_to_gemini(photo.temporary_file_path())
             data = json.loads(json_string)
             receipt_form = ReceiptForm({
-                'storeName': data.get('storeName', ''),
-                'storeAddress': data.get('storeAddress', ''),
-                'totalPrice': data.get('totalPrice', 0),
-                'date': data.get('date', timezone.now()),
+                'storeName': data.get('storeName'),
+                'storeAddress': data.get('storeAddress'),
+                'totalPrice': data.get('totalPrice'),
+                'date': parser.parse(data.get('date')),
             })
             if receipt_form.is_valid():
                 receipt = receipt_form.save(commit=False)
@@ -30,11 +30,11 @@ def upload_photo(request):
                 for item in data['items']:
                     product_data = {
                         'receipt': receipt,
-                        'name': item.get('name', ''),
-                        'nameEnglish': item.get('nameEnglish', ''),
-                        'nameChinese': item.get('nameChinese', ''),
-                        'price': item.get('price', 0.0),
-                        'discount': item.get('discount', 0.0)
+                        'name': item.get('name'),
+                        'nameEnglish': item.get('nameEnglish'),
+                        'nameChinese': item.get('nameChinese'),
+                        'price': item.get('price'),
+                        'discount': item.get('discount')
                     }
                     product_form = ProductForm(product_data)
                     if product_form.is_valid():
--- render/models.py
+++ render/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth import get_user_model
 
 
 class Receipt(models.Model):
-    storeName = models.CharField(max_length=200)
-    storeAddress = models.CharField(max_length=200)
+    storeName = models.CharField(max_length=200, null=True, blank=True)
+    storeAddress = models.CharField(max_length=200, null=True, blank=True)
     totalPrice = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateTimeField(null=True, default=timezone.now)
+    date = models.DateTimeField(null=True,  blank=True, default=timezone.now)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
@@ -17,11 +17,12 @@ class Receipt(models.Model):
 
 class Product(models.Model):
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    nameEnglish = models.CharField(max_length=200)
-    nameChinese = models.CharField(max_length=200)
-    discount = models.DecimalField(max_digits=10, decimal_places=2)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    nameEnglish = models.CharField(max_length=200, null=True, blank=True)
+    nameChinese = models.CharField(max_length=200, null=True, blank=True)
+    discount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
